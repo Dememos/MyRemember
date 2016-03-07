2a7100256775080d0dfc536db47e75fccf342291
Dememos <dememos@mail.ru>
2016-03-07 10:27:06 +0300

    new task

---
+++ app/src/main/java/com/dememos/admin/myremember/adapter/TabsFragmentAdapter.java
@@ -0,0 +1,48 @@
+package com.dememos.admin.myremember.adapter;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.dememos.admin.myremember.fragment.ExampleFragment;
+
+public class TabsFragmentAdapter extends FragmentPagerAdapter{
+
+    private String[] tabs;
+
+    public TabsFragmentAdapter(FragmentManager fm) {
+        super(fm);
+        tabs = new String[]{
+                "Tab 1",
+                "Напоминания",
+                "Tab 2"
+
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                return ExampleFragment.getInstance();
+            case 1:
+                return ExampleFragment.getInstance();
+            case 2:
+                return ExampleFragment.getInstance();
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
