21e5d5fef6536849e595579b4e6c27147054ed65
Dememos <dememos@mail.ru>
2016-03-09 17:56:49 +0300

    new

---
+++ app/src/main/java/com/dememos/admin/myremember/adapter/RemindListAdapter.java
@@ -0,0 +1,55 @@
+package com.dememos.admin.myremember.adapter;
+
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dememos.admin.myremember.R;
+import com.dememos.admin.myremember.dto.RemindDTO;
+
+import java.util.List;
+
+public class RemindListAdapter extends RecyclerView.Adapter<RemindListAdapter.RemindVeiwHolder>{
+
+    private List<RemindDTO> data;
+
+    public RemindListAdapter(List<RemindDTO> data) {
+        this.data = data;
+    }
+
+    public static class RemindVeiwHolder extends RecyclerView.ViewHolder{
+        CardView cardview;
+        TextView title;
+
+        public RemindVeiwHolder(View itemView){
+            super(itemView);
+            cardview = (CardView) itemView.findViewById(R.id.cardView);
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+
+
+    }
+
+
+    @Override
+    public RemindVeiwHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.remind_item, parent, false);
+        return new RemindVeiwHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RemindVeiwHolder holder, int position) {
+        RemindDTO item = data.get(position);
+        holder.title.setText(item.getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+}
--- app/src/main/java/com/dememos/admin/myremember/fragment/HistoryFragment.java
+++ app/src/main/java/com/dememos/admin/myremember/fragment/HistoryFragment.java
@@ -4,15 +4,22 @@ package com.dememos.admin.myremember.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dememos.admin.myremember.R;
+import com.dememos.admin.myremember.adapter.RemindListAdapter;
+import com.dememos.admin.myremember.dto.RemindDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HistoryFragment extends AbstractTabFragment {
-    private static final int LAYOUT = R.layout.fragment_example;
+    private static final int LAYOUT = R.layout.fragment_history;
 
 
     public static HistoryFragment getInstance(Context context){
@@ -29,9 +36,27 @@ public class HistoryFragment extends AbstractTabFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(LAYOUT,container,false);
+        view = inflater.inflate(LAYOUT, container, false);
+
+
+        RecyclerView cv = (RecyclerView) view.findViewById(R.id.recycleView);
+        cv.setLayoutManager(new LinearLayoutManager(context));
+        cv.setAdapter(new RemindListAdapter(createMockData()));
         return view;
 
+    }
+
+    private List<RemindDTO> createMockData() {
+        List<RemindDTO> data = new ArrayList<>();
+        data.add(new RemindDTO("Item 1"));
+        data.add(new RemindDTO("Item 2"));
+        data.add(new RemindDTO("Item 3"));
+        data.add(new RemindDTO("Item 4"));
+        data.add(new RemindDTO("Item 5"));
+        data.add(new RemindDTO("Item 6"));
+        data.add(new RemindDTO("Item 7"));
+
+        return data;
     }
 
     public void setContext(Context context) {
